fd28e2b5495f23187a2fc15e4610e81e2e764c71
Nicolas Allègre <nicolas.allegre@telecom-paris.fr>
2025-05-22 22:42:32 +0200

    Ajout Pipeline

---
+++ src/anomaly_lisa/pipeline.py
@@ -0,0 +1,294 @@
+"""BGDIA706 Projet fil-rouge, SAFRAN.
+
+Fichier pipeline.py : Défini le logger et le pipeline.
+
+MEMO vérif code : python -m pylama -l all --pydocstyle-convention pep257 *.py
+    # python -m pylama -l eradicate,mccabe,pycodestyle,pydocstyle,pyflakes,pylint,radon,vulture,isort --pydocstyle-convention pep257
+    # python -m mypy
+    # python -m autopep8 -a -p2 -r -v [-i | --diff]
+    # python -m isort --diff
+# MEMO console :
+import importlib
+importlib.reload(p)
+"""
+__author__ = ['Nicolas Allègre', 'Sarah Garcia', 'Luca Hachani', 'François-Xavier Morel']
+__date__ = '17/05/2025'
+__version__ = '0.2'
+
+###############################################################################
+# IMPORTS :
+# /* Modules standards */
+import argparse
+import logging
+import os
+import sys
+import time
+from typing import Final, Literal
+
+# /* Modules externes */
+# /* Module interne */
+
+
+###############################################################################
+# CONSTANTES :
+EXIT_OK: Final[int] = 0
+
+DEFAULT_SAVE_FOLDER: Final[str] = "results"
+DEFAULT_LOG_FOLDER: Final[str] = "logs"
+DEFAULT_LOG_FILENAME: Final[str] = "pipeline.log"
+DEFAULT_MODEL_FOLDER: Final[str] = "checkpoints"
+
+
+###############################################################################
+# CLASSES PROJET :
+class PipelineLogger:
+    """Le logger du projet.
+
+    ============
+    Attributs :
+        file                     le fichier de log si demandé ou stdout
+        logger (logging.Logger)  le logger
+
+    ============
+    Méthodes :
+        instance(msg, level,    **kwargs)  wrapper sur la classe logging.Logger
+            pour logger des messages.
+            => équivalent à instance.logger.info|error|...|(msg, **kwargs)
+        setLevel(level, handler)    wrapper sur setLevel pour metter à jour le niveau   
+            de log des differents logger.
+
+    ============
+    Exemples :
+        ```
+        # CAS : fonctionnement manuel
+        import Pipeline as TP
+        a=TP.PipelineLogger()  # =>Seulement stdout
+        a=TP.PipelineLogger(filepath='tt.txt')  # => sdtout + fichier
+        a=TP.PipelineLogger(filepath='tt.txt', is_print=False)  # =>Seulement fichier
+        a("test")
+        a("erreur", level=logging.ERROR)
+        a.setLevel(logging.INFO, "console")
+        a.setLevel(logging.WARNING, "file")
+        a("erreur", level=logging.INFO)  # Affiche en console et pas dans le fichier
+        # CAS : automatique
+        import Pipeline as TP
+        args = {"nolog":False, "logfile":sys.stdout, "log_is_print":True, "log_folder":"log/timestamp/"}
+        logger = TP.get_logger(args)
+        ```
+    """
+
+    def __init__(self, filepath: str | None = None, is_print: bool = True,
+                 logger_name: str = __name__):
+        """Initialise la classe et le logger.
+
+        :param (str) filepath:  fichier de log
+        :param (bool) is_print: indique si les logs s'affiche aussi dans stdout
+        """
+        self.file = sys.stdout
+        if filepath is not None:
+            self.file = filepath
+
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            "{asctime} - {name} - {levelname} - {message}",
+            style="{",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        if is_print:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(formatter)
+            # console_handler.setLevel("DEBUG")
+            logger.addHandler(console_handler)
+        if isinstance(self.file, str):
+            file_handler = logging.FileHandler(self.file, mode="a", encoding="utf-8")
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+        self.logger = logger
+
+    def __call__(self, msg: str, level: int = logging.INFO, **kwargs):
+        """Permet l'appel et wrapper sur la classe logging.Logger."""
+        log_fct = {
+            logging.DEBUG: self.logger.debug,
+            logging.INFO: self.logger.info,
+            logging.WARNING: self.logger.warning,
+            logging.ERROR: self.logger.error,
+            logging.CRITICAL: self.logger.critical,
+        }
+        log_fct.get(level, logging.INFO)(msg, **kwargs)
+
+    def setLevel(self, level: int, handler: Literal["all", "console", "file", "root"] = "all"):
+        """Permet de modifier le niveau de log des différentes instances.
+
+        :param (int) level:     niveau de log (cf. `loggin.logger.setLevel`)
+        :param (str) handler:   quel logger à modifier le niveua de log
+            "console" => le logger en charge de la console si existe
+            "file"    => le logger par fichier s'il existe
+            "root"    => la valeur par défaut des logger si non spécifié
+            "all"     => tout les logger
+        """
+        if handler in ("all", "root"):
+            self.logger.setLevel(level)
+        for x in self.logger.handlers:
+            update = False
+            is_file = isinstance(x, logging.FileHandler)
+            is_console = isinstance(x, logging.StreamHandler) and not is_file
+
+            update = (handler == "all") or \
+                (handler == "console" and is_console) or \
+                (handler == "file" and is_file)
+
+            # Mise à jour du niveau de log
+            if update:
+                x.setLevel(level)
+
+
+class Pipeline:
+    """Le pipeline du projet.
+    ...
+    """
+
+    name: str = "pipeline"
+
+    def __init__(self, logger: PipelineLogger | None):
+        """Initialise le pipeline."""
+        self.logger = logger
+        if logger is None:  # mode console
+            self.logger = PipelineLogger()
+
+
+###############################################################################
+# CLASSES PROJET :
+def get_logger(args: dict | None = None) -> PipelineLogger:
+    """Créer et configure un logger.
+
+    :param (dict) args:    les paramètres fournis
+    :return (PipelineLogger):     le logger initialisé
+    """
+    ###
+    # Gestion des arguments par défaut - compatible mode console ou mode Python
+    ###
+    if args is None:
+        args = {}
+
+    # nolog [--nolog] = (bool)
+    args["nolog"] = args.get("nolog", False)
+    # logfile [--logfile [FILENAME]] = défaut stdout, None si pas FILENAME
+    args["logfile"] = args["logfile"] if "logfile" in args else sys.stdout
+
+    filename = args["logfile"] if args["logfile"] is not None else DEFAULT_LOG_FILENAME
+    foldername = os.path.join(DEFAULT_LOG_FOLDER, str(int(time.time())))
+    # log_folder = (str) chemin du dossier de log
+    args["log_folder"] = args.get("log_folder", foldername)
+    # log_filepath = (str) None si pas de log en fichier
+    if args["nolog"] or not isinstance(filename, str):
+        args["log_filepath"] = None
+    else:
+        args["log_filepath"] = os.path.join(args["log_folder"], filename)
+    # log_is_print = (bool) local pour afficher les log
+    args["log_is_print"] = args.get("log_is_print", not args["nolog"])
+
+    ###
+    # Gestion du flux d'exécution
+    ###
+    # 0 - Création du logger
+    # print(f"{args["task"]=}, {args["log_filepath"]=}, {args["log_is_print"]=}, {args["nolog"]=}, {args["logfile"]=}")
+    logger = PipelineLogger(filepath=args["log_filepath"], is_print=args["log_is_print"])
+
+    return logger
+
+
+def run_process(args: dict | None = None) -> Pipeline:
+    """Exécute le déroulé d'un pipeline.
+
+    À modifier pour l'adapter à chaque agent IA
+
+    :param (dict) args:    les paramètres fournis
+    :return (PipelineLogger):     le logger initialisé
+    """
+    ###
+    # Gestion des arguments par défaut - compatible mode console ou mode Python
+    ###
+    if args is None:
+        args = {}
+
+    ###
+    # Gestion du flux d'exécution
+    ###
+    # 0 - Création du logger
+    logger = get_logger(args)
+
+    # 1 - Création du pipeline
+    pipeline = Pipeline(logger)
+
+    return pipeline
+
+
+###############################################################################
+# FONCTIONS MODE CONSOLE :
+def parse_args() -> argparse.Namespace:
+    """Gestion des arguments à modifier en fonction de l'agent IA.
+
+    ====
+    Arguments :
+        logfile [--logfile [FILENAME]] = défaut stdout, None si pas FILENAME
+        nolog [--nolog] = (bool)
+
+    :param (str) args: les arguments données au programme
+    :return (argparse.Namespace):   les arguments parsés
+    """
+    # 1 - Définition des listes de choix :
+    # 2 - Création du parseur à arguments:
+    parser = argparse.ArgumentParser(prog="Pipeline",
+                                     description="Gère les différents enchaînement des agents.")
+    # 3 - Définition des arguments :
+    parser.add_argument("--logfile", nargs='?', type=argparse.FileType("w"),
+                        default=sys.stdout, help="sortie du programme")
+    parser.add_argument("--nolog", action='store_true',
+                        help="désactive les log")
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace) -> int:
+    """Exécute le flux d'exécution des tâches pour l'agent.
+
+    :param args:    les paramètres fournis en ligne de commande
+    :return (int):  le code retour du programme
+    """
+    exit_value: int = EXIT_OK
+    # Gestion particulière des args externes si besoin
+    print(args)
+
+    # Exécution
+    start_time = time.time()
+    pipeline = run_process(vars(args))
+    print(f'\n--- {time.time() - start_time} seconds ---')
+
+    return exit_value
+
+
+###############################################################################
+if __name__ == "__main__":
+    print(sys.argv)
+    sys.exit(main(parse_args()))
+# end if
+
+####
+# TEST pour mémo
+# TP.run()
+# TP.run({"task":"train"})
+# TP.run({"nolog":False})
+# TP.run({"nolog":True})
+# TP.run({"logfile":sys.stderr})
+# TP.run({"logfile":None})
+# TP.run({"logfile":sys.stdout, "nolog":True})
+# TP.run({"logfile":"tt.txt"})
+# TP.run({"logfile":"tt.txt", "nolog":True})
+# TP.run({"log_folder":"lll", "logfile":sys.stdout})
+# TP.run({"log_folder":"lll", "logfile":"tt.txt"})
+# TP.run({"log_is_print":False})
+# TP.run({"log_is_print":False, "nolog":True})
+# TP.run({"log_is_print":True})
+# TP.run({"log_is_print":True, "nolog":True})
+# TP.run({"logfile":"tt.txt", "log_filepath":"rr"})
---
+++ src/anomaly_lisa/pipeline.sh
@@ -0,0 +1,69 @@
+#! /bin/sh
+
+###############################################################################
+# CONSTANTES :
+################
+# Les différents types d'environnement Python
+GLOBAL="myenv_anomalib"
+LISA="myenv_lisa"
+
+# Les chemins globaux
+miniconda="~/miniconda3/bin/activate"
+################
+
+###############################################################################
+# FONCTIONS :
+function read_config_file() {
+    # Lecture d'un fichier d'entrée de config afin de savoir quel pipeline à exécuter.
+    # (sinon à récupérer par les arguments en ligne de commande)
+    # $1 = nom du fichier config
+    # return => $? = les informations d'exécution (quel agent, quel enchaînement, quel fichier IN, etc.)
+}
+
+function read_config_agent() {
+    # Lecture du fichier conf d'un agent
+    # $1 = nom du fichier config
+    # return => $? = les informations nécessaire à lancer l'agent (comme son env Python)
+}
+
+###############################################################################
+# 0 - Gestion des arguments
+config_path = $1
+
+# 1 - Activation de l'environnement Python
+. ~/miniconda3/bin/activate
+
+# 2 - Récupértion des tâches à accomplir et du pipeline
+config_pipeline=read_config_file $config_path
+
+# 3 - Récupération des différents conf des agents à exécuter
+# for chaque agent demandé charger son fichier de conf
+
+# 4 - Décision d'action à faire
+
+
+# Exemple concept fonctionnement partie python
+
+################
+## ENV ANOMALIB
+conda activate $GLOBAL
+
+python --version
+python -m pip list |grep -E "numpy|torch"
+python pipeline.py -h
+
+conda deactivate
+################
+################
+## ENV LISA
+conda activate $LISA
+cd  experiments/LISA/
+python --version
+python -m pip list |grep -E "numpy|torch"
+ls -l chat.py
+
+conda deactivate
+################
+
+# 99 - Dé-activation de l'environnement Python
+conda deactivate
